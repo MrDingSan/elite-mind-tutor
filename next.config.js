1690faf4fd4358445770324750295aa8cb844017
MrDingSan <dingsanxiansheng@gmail.com>
2025-04-29 22:00:38 +0800

    Initial commit with Next.js setup

--- next.config.js
+++ next.config.js
@@ -1,60 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
-  experimental: {
-    appDir: true,
-  },
-  trailingSlash: true,
-  webpack: (config, { isServer }) => {
-    // Optimize bundle size
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 20000,
-          maxSize: 24400000, // 24.4MB
-          cacheGroups: {
-            default: false,
-            vendors: false,
-            framework: {
-              chunks: 'all',
-              name: 'framework',
-              test: /[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
-              priority: 40,
-              enforce: true,
-            },
-            lib: {
-              test: /[\\/]node_modules[\\/]/,
-              chunks: 'all',
-              name(module) {
-                const match = module.context?.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/);
-                if (!match) return 'vendor';
-                const packageName = match[1];
-                return `npm.${packageName.replace('@', '')}`;
-              },
-              priority: 30,
-            },
-            commons: {
-              name: 'commons',
-              minChunks: 2,
-              priority: 20,
-            },
-            shared: {
-              name: false,
-              priority: 10,
-              minChunks: 2,
-              reuseExistingChunk: true,
-            },
-          },
-        },
-      };
-    }
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': require('path').resolve(__dirname, 'src'),
