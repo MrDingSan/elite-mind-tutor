fa74ab33f9a3c908e5e057883450feb1c0a4b008
MrDingSan <dingsanxiansheng@gmail.com>
2025-05-10 09:02:30 +0200

    fix: cloudflare d1 adapter and autoprefixer for cloudflare pages

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaD1 } from '@prisma/adapter-d1'
+import { D1Database } from '@cloudflare/workers-types'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
 export const prisma = global.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DB as string,
+    },
+  },
   adapter: new PrismaD1(process.env.DB as D1Database),
 })
 
