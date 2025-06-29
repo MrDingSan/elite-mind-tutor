2acc0a5fbfb9947ff9a72105979e2551419aaa9d
MrDingSan <dingsanxiansheng@gmail.com>
2025-06-29 23:17:07 +0800

    Fix TypeScript build error: Remove unused imports from biology tuition page - Remove FaGraduationCap, FaUsers, and FaAward unused imports - Fixes Cloudflare deployment build failure

--- src/app/biology-tuition/page.tsx
+++ src/app/biology-tuition/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FaGraduationCap, FaBook, FaChartLine, FaUsers, FaAward, FaMicroscope, FaCheckCircle, FaArrowRight, FaFlask } from 'react-icons/fa';
+import { FaBook, FaChartLine, FaMicroscope, FaCheckCircle, FaArrowRight, FaFlask } from 'react-icons/fa';
 
 const biologyTopics = [
   {
